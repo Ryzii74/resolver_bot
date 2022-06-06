04779fb35ddb68ba03079fe9f66032c79e6cfd45
boronin <ryzii@yandex.ru>
2022-06-07 00:33:11 +0500

    fix parsing

--- libs/phrasesArray.js
+++ libs/phrasesArray.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = fs.readFileSync(path.join(__dirname, '../config/phrases.txt')).toString().split('\n');
+module.exports = fs.readFileSync(path.join(__dirname, '../config/phrases.txt')).toString().split('\r');
