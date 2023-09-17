76bb96fd9a39657f65715488ae93d1d7ca9ee4a5
Ryzii74 <Ryzii@yandex.ru>
2023-09-17 22:12:58 +0200

    wiki-словарь

--- commands/frazeologism.js
+++ commands/frazeologism.js
@@ -6,7 +6,7 @@ module.exports = async (msg) => {
     const {text} = msg;
     const directPhrases = phrases.filter(phrase =>  new RegExp(`(^|\\s)${text}(\\s|$)`).test(phrase));
     const allPhrases = phrases.filter(phrase => !directPhrases.includes(phrase) && phrase.includes(text));
-    const wikiPhrases = wikislovar.filter(phrase => phrase.includes(phrase));
+    const wikiPhrases = wikislovar.filter(phrase => phrase.includes(text));
 
     if (!directPhrases.length && !allPhrases.length) {
         msg.addTextResponse("Ничего не найдено!");
