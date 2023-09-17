09e29fad687c4c7a3df36a674cfeeb23140023b0
Ryzii74 <Ryzii@yandex.ru>
2023-09-17 22:04:37 +0200

    wiki-словарь

--- commands/frazeologism.js
+++ commands/frazeologism.js
@@ -1,16 +1,19 @@
 const phrases = require('../libs/phrasesArray');
+const wikislovar = require('../config/wikislovar.json');
 
 module.exports = async (msg) => {
     // стемминг???
     const {text} = msg;
     const directPhrases = phrases.filter(phrase =>  new RegExp(`(^|\\s)${text}(\\s|$)`).test(phrase));
     const allPhrases = phrases.filter(phrase => !directPhrases.includes(phrase) && phrase.includes(text));
+    const wikiPhrases = wikislovar.filter(phrase => phrase.includes(phrase));
 
     if (!directPhrases.length && !allPhrases.length) {
         msg.addTextResponse("Ничего не найдено!");
         return;
     }
 
-    msg.addTextResponse(directPhrases.join('\n'));
-    msg.addTextResponse(allPhrases.join('\n'));
+    msg.addTextResponse(['ВИКИСЛОВАРЬ', ...wikiPhrases].join('\n'));
+    msg.addTextResponse(['ПОЛНЫЕ СОВПАДЕНИЯ СЛОВ', ...directPhrases].join('\n'));
+    msg.addTextResponse(['ЧАСТИЧНЫЕ СОВПАДЕНИЯ СЛОВ', ...allPhrases].join('\n'));
 };
