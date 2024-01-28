4c646063f64c861e8824e7cb7a5d19a6119f8ea3
Ryzii74 <Ryzii@yandex.ru>
2024-01-28 07:19:47 +0100

    матрицы

--- commands/matrix.js
+++ commands/matrix.js
@@ -1,26 +1,35 @@
 const dictionaryArray = require('../actions/sources/dictionaryArray')();
 
+const DEFAULT_LENGTH = 3;
+
 module.exports = async (msg) => {
     let {text} = msg;
     const words = text.split(' ');
+    const subwordLength = getSubwordLength(words[words.length - 1]);
+    console.log(subwordLength)
     const correctWords = dictionaryArray.filter(word => {
+        const matches = {0: [], 1: []};
         for (let i = 0; i < 2; i++) {
             const wordToMatch = words[i];
-            let isMatch = false;
-            for (let j = 0; j < wordToMatch.length - 3; j++) {
-                const substr = wordToMatch.substring(j, j + 3);
+            for (let j = 0; j <= wordToMatch.length - subwordLength; j++) {
+                const substr = wordToMatch.substring(j, j + subwordLength);
                 if (word.includes(substr)) {
-                    isMatch = true;
+                    matches[i].push(substr);
                 }
             }
 
-            if (!isMatch) {
+            if (!matches[i].length) {
                 return false;
             }
         }
 
-        return true;
+        return matches[0].some(el => matches[1].includes(el));
     });
 
     msg.addAnswersResponse(correctWords);
 };
+
+function getSubwordLength(lastWord) {
+    const possibleNumber = Number(lastWord);
+    return Number.isNaN(possibleNumber) ? DEFAULT_LENGTH : possibleNumber;
+}
