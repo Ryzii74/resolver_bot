11db403687947d605b2c25b6629cafc8c4bf540d
Ryzii74 <Ryzii@yandex.ru>
2024-03-10 21:04:34 +0100

    phrase with several words

--- commands/frazeologism.js
+++ commands/frazeologism.js
@@ -4,9 +4,10 @@ const wikislovar = require('../data/wikislovar.json');
 module.exports = async (msg) => {
     // стемминг???
     const {text} = msg;
-    const directPhrases = phrases.filter(phrase =>  new RegExp(`(^|\\s)${text}(\\s|$)`).test(phrase));
-    const allPhrases = phrases.filter(phrase => !directPhrases.includes(phrase) && phrase.includes(text));
-    const wikiPhrases = wikislovar.filter(phrase => phrase.includes(text));
+    const words = text.split(' ');
+    const directPhrases = phrases.filter(phrase => words.every(word => new RegExp(`(^|\\s)${word}(\\s|$)`).test(phrase)));
+    const allPhrases = phrases.filter(phrase => words.every(word => !directPhrases.includes(phrase) && phrase.includes(word)));
+    const wikiPhrases = wikislovar.filter(phrase => words.every(word => phrase.includes(word)));
 
     if (!wikiPhrases.length && !directPhrases.length && !allPhrases.length) {
         msg.addTextResponse("Ничего не найдено!");
