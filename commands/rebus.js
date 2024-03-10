e16d22fef667beac6280e405f7c2224cf7367733
Ryzii74 <Ryzii@yandex.ru>
2024-03-10 19:14:17 +0100

    rebus several words

--- commands/rebus.js
+++ commands/rebus.js
@@ -3,13 +3,17 @@ const resolver = require('./resolvers/mask');
 module.exports = async (msg) => {
     const words = msg.text.split(' ');
 
-    const correctWords1 = resolver(`*${words[0]}*${words[1]}*`);
-    const correctWords2 = resolver(`*${words[1]}*${words[0]}*`);
+    const correctWords = [];
+    const permutations = getAllPermutations(words);
+    permutations.forEach(array => {
+        let maskStr = `*${array.join('*')}*`;
+        correctWords.push(...resolver(maskStr));
+    })
 
     const correctWordsLong = [];
     const correctWordsShort = [];
     const length = words.join('').length;
-    [...correctWords1, ...correctWords2].forEach(word => {
+    correctWords.forEach(word => {
        let wordPrepared = word;
        words.sort((a,b) => b.length - a.length)
            .forEach(el => { wordPrepared = wordPrepared.replace(el, el.toUpperCase()); });
@@ -19,8 +23,28 @@ module.exports = async (msg) => {
            correctWordsShort.push(wordPrepared);
        }
     });
+
     correctWordsLong.sort((a,b) => b.length - a.length);
     correctWordsShort.sort((a,b) => b.length - a.length);
     msg.addAnswersResponse(correctWordsLong, '\n', 'ДЛИННЫЕ');
     msg.addAnswersResponse(correctWordsShort, '\n', 'КОРОТКИЕ');
 };
+
+function getAllPermutations(array) {
+    const results = [];
+
+    if (array.length === 1) {
+        results.push(array);
+        return results;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        const firstEl = array[i];
+        const elementsLeft = [...array.slice(0, i), ...array.slice(i + 1)];
+        const innerPermutations = getAllPermutations(elementsLeft);
+        for (let j = 0; j < innerPermutations.length; j++) {
+            results.push([firstEl, ...innerPermutations[j]]);
+        }
+    }
+    return results;
+}
