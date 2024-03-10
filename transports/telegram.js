070f218f4490a86f22b73d15891796f7eb992469
Ryzii74 <Ryzii@yandex.ru>
2024-03-11 00:00:47 +0100

    response log

--- transports/telegram.js
+++ transports/telegram.js
@@ -52,6 +52,7 @@ async function sendTextMessage(userId, text, isRepeated) {
         .replaceAll('!', '\\!')
         .replaceAll('.', '\\.')
         .replaceAll('-', "\\'");
+    console.log(`Отправка сообщения пользователю ${userId}: ${preparedText}`)
     await bot.sendMessage(userId, preparedText, { parse_mode: 'MarkdownV2' });
   } catch (err) {
     console.log(err.code, err.response.body);
