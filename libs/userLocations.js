9379b4313882a5549437a9d2f4586780a673634e
Ryzii74 <Ryzii@yandex.ru>
2024-06-20 01:05:48 +0500

    log on user location update

--- libs/userLocations.js
+++ libs/userLocations.js
@@ -2,6 +2,7 @@ const userLocations = {};
 
 module.exports = {
     set: (userId, location) => {
+        console.log('Updated user location', userId, location);
         userLocations[userId] = location;
     },
 
