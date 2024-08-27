ec6d47c124795cc0ba38502911df9f6a3b290522
Ryzii74 <Ryzii@yandex.ru>
2024-08-27 19:19:48 +0300

    tests association

---
+++ test/actions/association.test.js
@@ -0,0 +1,29 @@
+const sendCommand = require('../utils/sendCommand');
+
+require('../toBeEmptyResponse');
+
+describe('association', () => {
+    beforeAll(async () => {
+        await sendCommand('/association');
+    });
+
+    test('ассоциации без результатов', async () => {
+        const res = await sendCommand('хурма таджик');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toBeEmptyResponse();
+    });
+
+    test('ассоциации на одно слово', async () => {
+        const res = await sendCommand('любовь');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toHaveLength(100);
+        expect(res[0]).toContain('счастье');
+    });
+
+    test('ассоциации на два слова', async () => {
+        const res = await sendCommand('любовь дом');
+        expect(res).toHaveLength(1);
+        expect(res[0].length).toBeGreaterThan(5);
+        expect(res[0]).toContain('счастье');
+    });
+});
