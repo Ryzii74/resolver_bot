c9025f1ecc3121a5e61ee85a57e3737b56e7ed1d
Ryzii74 <Ryzii@yandex.ru>
2024-08-27 21:14:11 +0300

    tests meta

---
+++ test/actions/meta.test.js
@@ -0,0 +1,31 @@
+const sendCommand = require('../utils/sendCommand');
+
+require('../toBeEmptyResponse');
+
+describe('metagramma', () => {
+    beforeAll(async () => {
+        await sendCommand('/meta');
+    });
+
+    test('без результатов', async () => {
+        const res = await sendCommand('фывапро');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toBeEmptyResponse();
+    });
+
+    test('нашелся 1 результат', async () => {
+        const res = await sendCommand('ломбырд');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toEqual(['ломбард']);
+    });
+
+    test('нашлось несколько результатов', async () => {
+        const res = await sendCommand('домна');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toEqual([
+            'дойна',
+            'домра',
+            'донна',
+        ]);
+    });
+});
