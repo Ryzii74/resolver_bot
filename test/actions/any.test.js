fcb060e0eb5346cea3bf4c48c4eebb85854b9138
Ryzii74 <Ryzii@yandex.ru>
2024-08-27 21:34:02 +0300

    tests any

---
+++ test/actions/any.test.js
@@ -0,0 +1,34 @@
+const sendCommand = require('../utils/sendCommand');
+
+require('../toBeEmptyResponse');
+
+describe('anagramma', () => {
+    beforeAll(async () => {
+        await sendCommand('/any');
+    });
+
+    test('без результатов', async () => {
+        const res = await sendCommand('фывапро');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toBeEmptyResponse();
+    });
+
+    test('что-то нашлось', async () => {
+        const res = await sendCommand('хурма');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toEqual([
+            'амур',
+            'мархур',
+            'муар',
+            'мура',
+            'муха',
+            'ухма',
+            'фурма',
+            'храм',
+            'хула',
+            'хурма',
+            'хурма',
+            'шаурма',
+        ]);
+    })
+});
