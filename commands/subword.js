bef2e45467bc44d83f7b84beacf03952f784ed1c
Ryzii74 <Ryzii@yandex.ru>
2024-08-28 11:17:28 +0300

    tests subword

--- commands/subword.js
+++ commands/subword.js
@@ -5,8 +5,15 @@ const dictionaryArraySource = require('../actions/sources/dictionaryArray');
 const secondWordFormatter = require('../actions/formatters/secondWord');
 
 module.exports = async (msg) => {
-    const {text} = msg;
+    const { text } = msg;
     const resolver = new Resolver(wordSource(text), dictionaryArraySource, task, secondWordFormatter);
-    const answers = await resolver.resolve();
+    const answersRaw = await resolver.resolve();
+    const answers = answersRaw.filter(Boolean);
+
+    if (!answers.length) {
+        msg.addTextResponse("Нет результатов");
+        return;
+    }
+
     msg.addAnswersResponse(answers, ' ');
 };
---
+++ test/actions/subword.test.js
@@ -0,0 +1,21 @@
+const sendCommand = require('../utils/sendCommand');
+
+require('../toBeEmptyResponse');
+
+describe('rebus', () => {
+    beforeAll(async () => {
+        await sendCommand('/subword');
+    });
+
+    test('без результатов', async () => {
+        const res = await sendCommand('рыг');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toBeEmptyResponse();
+    });
+
+    test('что-то нашлось', async () => {
+        const res = await sendCommand('фывапро');
+        expect(res).toHaveLength(1);
+        expect(res[0]).toEqual(['ар вар во по по про про ро фа']);
+    });
+});
