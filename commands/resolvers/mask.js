710b217c18ca20456985a00710fc9d005cf96f42
Ryzii74 <Ryzii@yandex.ru>
2025-02-21 00:10:21 +0300

    fix mask

--- commands/resolvers/mask.js
+++ commands/resolvers/mask.js
@@ -2,6 +2,6 @@ const dictionary = require("../../libs/dictionary");
 
 module.exports = (text) => {
     const wordsArray = dictionary.getArray(text);
-    const reg = new RegExp('^' + text.replace(/\*/g, '\\S*').replace(/\?/g, '\\S') + '$', 'gi');
-    return wordsArray.filter(word => reg.exec(word));
+    const reg = new RegExp('^' + text.replace(/\*/g, '\\S*').replace(/\?/g, '\\S') + '$', 'i');
+    return wordsArray.filter(word => reg.test(word));
 };
