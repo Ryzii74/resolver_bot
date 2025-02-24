22bbe18ad625e3491a7d07af2c32278cf01bf5b7
Ryzii74 <Ryzii@yandex.ru>
2025-02-24 23:26:58 +0300

    maskword

---
+++ commands/maskWord.js
@@ -0,0 +1,21 @@
+const dictionary = require("../libs/dictionary");
+module.exports = function (msg) {
+    const {text} = msg;
+
+    const wordsArray = dictionary.getArray(text);
+    const wordsObject = dictionary.getObject(text);
+    const correctWords = [];
+    const reg = new RegExp('^' + text.replace(/\?/g, '\(\\S*\)') + '$', 'i');
+    wordsArray.forEach(word => {
+        const res = reg.exec(word);
+        if (res && wordsObject[res[1]]) {
+            correctWords.push(word.replace(res[1], res[1].toUpperCase()));
+        }
+    });
+
+    if (correctWords.length) {
+        msg.addAnswersResponse(correctWords, '\n');
+    } else {
+        msg.addTextResponse("Нет результатов");
+    }
+};
