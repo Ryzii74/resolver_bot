c52605bc77bd95a0cf06da93b192dbb810f3ef83
Ryzii74 <Ryzii@yandex.ru>
2025-03-01 13:17:54 +0300

    mask with digits

--- commands/resolvers/mask.js
+++ commands/resolvers/mask.js
@@ -1,7 +1,39 @@
 const dictionary = require("../../libs/dictionary");
 
 module.exports = (text) => {
+    const isCheckDigits = /[1-9]/.test(text);
+    const digitsToCheck = isCheckDigits ? prepareDigitsToCheck(text) : [];
+
     const wordsArray = dictionary.getArray(text);
-    const reg = new RegExp('^' + text.replace(/\*/g, '\\S*').replace(/\?/g, '\\S') + '$', 'i');
-    return wordsArray.filter(word => reg.test(word));
+    const reg = new RegExp('^' + text.replace(/\*/g, '\\S*').replace(/[?1-9]/g, '\\S') + '$', 'i');
+    return wordsArray.filter(word => {
+        if (!reg.test(word)) {
+            return false;
+        }
+
+        return digitsToCheck.every(symbols => {
+            const letter = word[symbols[0]];
+            return symbols.map(symbol => word[symbol]).every(s => s === letter);
+        });
+    });
 };
+
+function prepareDigitsToCheck(text) {
+    const sameSymbols = {};
+    text.split('').forEach((letter, index) => {
+        if (/[1-9]/.test(letter)) {
+            sameSymbols[letter] = sameSymbols[letter] || [];
+            sameSymbols[letter].push(index);
+        }
+    });
+    return Object.keys(sameSymbols)
+        .map(letter => {
+            const symbols = sameSymbols[letter];
+            if (symbols.length < 2) {
+                return;
+            }
+
+            return symbols;
+        })
+        .filter(Boolean);
+}
