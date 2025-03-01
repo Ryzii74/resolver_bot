aa896b844f2e10d1df32d096f57b7731b0eb3feb
Ryzii74 <Ryzii@yandex.ru>
2025-03-01 18:43:12 +0300

    гапоифика по картинам

--- commands/gapoifika.js
+++ commands/gapoifika.js
@@ -1,12 +1,17 @@
 const films = require('../actions/sources/filmsArray')();
 const books = require('../actions/sources/booksArray')();
+const paintings = require('../actions/sources/paintingsArray')();
 
 module.exports = async (msg) => {
     const {text} = msg;
 
     const foundBooks = books.filter(book => isGapoifika(text, book.name));
     const foundFilms = films.filter(film => isGapoifika(text, film.name));
-    if (foundBooks.length || foundFilms.length) {
+    const foundPaintings = paintings.filter(painting => isGapoifika(text, painting.name));
+    if (foundBooks.length || foundFilms.length || foundPaintings.length) {
+        if (foundPaintings.length) {
+            msg.addAnswersResponse(foundPaintings.map(el => el.name), '\n', 'Картины');
+        }
         if (foundBooks.length) {
             msg.addAnswersResponse(foundBooks.map(el => el.name), '\n', 'Книги');
         }
