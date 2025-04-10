28a7698292a67084533146af6d936df1dc12b00a
Ryzii74 <Ryzii@yandex.ru>
2025-04-10 16:01:47 +0300

    sborkaLine update

--- commands/sborka.js
+++ commands/sborka.js
@@ -1,30 +1,4 @@
 const dictionary = require("../libs/dictionary");
-const {getWordsFromLine} = require('../utils/getWords');
-
-module.exports.sborkaLine = (msg) => {
-    const {text} = msg;
-    const rawLines = text.split('\n');
-
-    const numbers = rawLines[rawLines.length - 1].split(' ').map(Number);
-
-    const letters = rawLines.slice(0, rawLines.length - 1).join('').replaceAll(' ', '');
-    const lines = numbers.map(_ => letters);
-    const answers = [`По номеру буквы: ${getWordsFromLine(getByNumbers(lines, numbers))}`];
-
-    const numbersWithoutZero = numbers.filter(Boolean);
-    if (Math.min(...numbersWithoutZero) === 1 && Math.max(...numbersWithoutZero) === numbersWithoutZero.length) {
-        let wordLetters = [];
-        numbers.forEach((number, index) => {
-            if (number === 0) return;
-            wordLetters[number - 1] = letters[index];
-        });
-
-        const word = wordLetters.map(letter => letter || '?').join('');
-        answers.push(`По позиции цифры: ${getWordsFromLine(word)}`);
-    }
-
-    msg.addAnswersResponse(answers);
-};
 
 module.exports = async (msg) => {
     const {text} = msg;
--- commands/sborkaLine.js
+++ commands/sborkaLine.js
@@ -9,8 +9,19 @@ module.exports = (msg) => {
 
     const letters = rawLines.slice(0, rawLines.length - 1).join('').replaceAll(' ', '');
     const lines = numbers.map(_ => letters);
+    const answers = [`По номеру буквы: ${getWordsFromLine(getByNumbers(lines, numbers))}`];
 
-    const result = getByNumbers(lines, numbers);
+    const numbersWithoutZero = numbers.filter(Boolean);
+    if (Math.min(...numbersWithoutZero) === 1 && Math.max(...numbersWithoutZero) === numbersWithoutZero.length) {
+        let wordLetters = [];
+        numbers.forEach((number, index) => {
+            if (number === 0) return;
+            wordLetters[number - 1] = letters[index];
+        });
 
-    msg.addTextResponse(getWordsFromLine(result));
+        const word = wordLetters.map(letter => letter || '?').join('');
+        answers.push(`По позиции цифры: ${getWordsFromLine(word)}`);
+    }
+
+    msg.addAnswersResponse(answers);
 };
