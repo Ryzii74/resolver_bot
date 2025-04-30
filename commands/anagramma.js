e4a45e12f5dcdb64ef8c5eb4b839487c4306dacd
Ryzii74 <Ryzii@yandex.ru>
2025-04-30 19:34:03 +0300

    anagramma two words

--- commands/anagramma.js
+++ commands/anagramma.js
@@ -1,7 +1,83 @@
 const resolver = require('./resolvers/anagramma');
+const dictionary = require("../libs/dictionary");
 
 module.exports = async (msg) => {
     let {text} = msg;
     const correctWords = resolver(text);
-    msg.addAnswersResponse(correctWords);
+    const wordsArray = dictionary.getArray(text);
+    const wordPairs = isAnagramCombo(text, wordsArray)
+    msg.addAnswersResponse(wordPairs.map(([w1, w2]) => `${w1} ${w2}`), undefined, 'Пары слов');
+    msg.addAnswersResponse(correctWords, undefined, 'Одно слово');
 };
+
+function isAnagramCombo(text, wordsArray) {
+    const lettersCount = countLetters(text);
+
+    // Подсчёт букв в строке
+    function countLetters(word) {
+        const count = {};
+        for (let char of word) {
+            count[char] = (count[char] || 0) + 1;
+        }
+        return count;
+    }
+
+    // Проверка, можно ли из lettersCount составить wordCount
+    function canForm(wordCount, lettersCount) {
+        for (let char in wordCount) {
+            if ((lettersCount[char] || 0) < wordCount[char]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Вычитание одного словаря частот из другого
+    function subtractCounts(count1, count2) {
+        const result = {};
+        for (let char in count1) {
+            if (!count1[char]) return null;
+
+            result[char] = count1[char] - (count2[char] || 0);
+            if (result[char] < 0) return null; // Нельзя вычитать больше, чем есть
+            if (result[char] === 0) delete result[char];
+        }
+        return result;
+    }
+
+    // Проверка на полное совпадение частот
+    function isEqualCount(count1, count2) {
+        const keys1 = Object.keys(count1);
+        const keys2 = Object.keys(count2);
+        if (keys1.length !== keys2.length) return false;
+        for (let key of keys1) {
+            if (count1[key] !== count2[key]) return false;
+        }
+        return true;
+    }
+
+    // Предварительно фильтруем слова, которые вообще можно собрать
+    const validWords = wordsArray.filter(word => canForm(countLetters(word), lettersCount));
+
+    // Перебор подходящих пар слов
+    const pairs = [];
+    for (let i = 0; i < validWords.length; i++) {
+        const word1 = validWords[i];
+        const count1 = countLetters(word1);
+
+        const remainingLetters = subtractCounts(lettersCount, count1);
+        if (!remainingLetters) continue;
+        console.log(word1, remainingLetters)
+
+        for (let j = i + 1; j < validWords.length; j++) {
+            const word2 = validWords[j];
+            const count2 = countLetters(word2);
+
+            if (isEqualCount(remainingLetters, count2)) {
+                pairs.push([word1, word2]);
+            }
+        }
+    }
+
+    return pairs;
+}
--- utils/getCombinations.js
+++ utils/getCombinations.js
@@ -18,6 +18,30 @@ function getCombinations(optionsArray) {
     }
 
     return combinations;
-};
+}
 
 module.exports = getCombinations;
+
+module.exports.getGenerations = getGenerations;
+function getGenerations(array) {
+    const optionsSum = array.reduce((sum, el) => sum * array.length, 1);
+
+    let start = 1;
+    const dividers = array
+        .map(_ => {
+            start *= array.length;
+            return start;
+        });
+
+    const combinations = [];
+    for (let i = 0; i < optionsSum; i++) {
+        const combination = [i % dividers[0]];
+        for (let j = 0; j < optionsArray.length - 1; j++) {
+            combination.push(Math.floor(i / dividers[j]) % optionsArray[j + 1]);
+        }
+        combinations.push(combination);
+    }
+
+    return combinations;
+}
+
