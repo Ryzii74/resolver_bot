ed77d41ef0895e8a93d87a8337b03f2a38d1f801
Ryzii74 <Ryzii@yandex.ru>
2025-09-12 23:49:44 +0300

    vigenere brutforce

---
+++ commands/vigenereBrutforce.js
@@ -0,0 +1,49 @@
+const {vigenereDecrypt} = require('./vigenere');
+const {lettersRu} = require('../data/letters');
+const dictionary = require("../libs/dictionary");
+
+const brutforce = async (msg) => {
+  const {text} = msg;
+
+  const words = dictionary.getObject(text);
+
+  const all = generateAll(16, true);
+  console.log('Всего вариантов', all.length);
+  const response = [];
+  for (let i = 0; i < all.length; i++) {
+    const key = all[i];
+    const decoded = vigenereDecrypt(text, key);
+    if (words[decoded]) {
+      response.push(`${decoded} (${key})`);
+    }
+  }
+
+  msg.addAnswersResponse(response);
+};
+
+function generateAll(maxLength, checkWords = true) {
+  if (checkWords) {
+    const words = dictionary.getArray('привет');
+    return words.filter(word => word.length && word.length <= maxLength);
+  }
+
+  const results = [];
+
+  function backtrack(current) {
+    if (current.length > 0 && current.length <= maxLength) {
+      results.push(current);
+    }
+    if (current.length === maxLength) return;
+
+    for (const letter of lettersRu) {
+      backtrack(current + letter);
+    }
+  }
+
+  backtrack("");
+  return results;c
+}
+
+module.exports = brutforce;
+
+brutforce({ text: 'гчэдэ', addAnswersResponse: console.log });
